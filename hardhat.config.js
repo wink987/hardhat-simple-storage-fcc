44e30e386f6f114044c4895e3a968528f7003733
mr_zhang <curlking@outlook.com>
2024-03-07 19:34:47 +0800

    <refresh>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config() //dotenv用来加载环境变量
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomicfoundation/hardhat-verify")
+require("hardhat-deploy")
+const { ProxyAgent, setGlobalDispatcher } = require("undici")
+const proxyAgent = new ProxyAgent("http://127.0.0.1:7890")
+setGlobalDispatcher(proxyAgent)
 /** @type import('hardhat/config').HardhatUserConfig */
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
     defaultNetwork: "hardhat",
@@ -11,6 +17,10 @@ module.exports = {
             url: SEPOLIA_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 11155111,
+            gasPrice: 1500000000,
         },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,44 @@
-const { ethers } = require("hardhat") //hardhat的好处是可以自动识别contracts包下面的已经编译好的合约，而ethers不可以
-
+const { ethers, run, network } = require("hardhat") //hardhat的好处是可以自动识别contracts包下面的已经编译好的合约，而ethers不可以
+require("dotenv").config()
 async function mian() {
-    const SimpleStorageFactory = await ethers.getContractFactory(
-        "SimpleStorage"
-    )
+    const SimpleStorageFactory =
+        await ethers.getContractFactory("SimpleStorage")
     console.log("Deploying Contract  ....")
     const simpleStorage = await SimpleStorageFactory.deploy()
-    console.log(`Deployed  Contract  to  : ${simpleStorage.target} `)
+    await simpleStorage.waitForDeployment()
+    console.log(`Deployed  Contract  to  : ${simpleStorage.target} `) //target相当于address
+    //console.log(network.config)//通过输出的chainid可以看到连接的哪个测试网络
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        //只有当在sepolia测试网才可以验证，并且要有以太坊浏览器的api接口，hardhat是本地的区块链，所以无法在以太坊浏览器验证
+        //x===x  与  x==x作用相同
+        console.log("waiting  for  block  txes...")
+        //await simpleStorage.deploymentTransaction().wait(6)
+        await verify(simpleStorage.target, [])
+    }
+    const currentValue = await simpleStorage.retrieve() //retrieve函数只是获得值，而不是修改值，故不需要等待区块，因此就不需要用await和wait
+    console.log(`Current Value is :  ${currentValue}`)
+    //update the value
+    const transactionResponse = await simpleStorage.store(9) //////////////////////////////////////////////////////////////////
+    await transactionResponse.wait(1)
+    const updatevalue = await simpleStorage.retrieve()
+    console.log(`The update value is : ${updatevalue}`)
+}
+async function verify(ContractAddress, args) {
+    //args代表构造函数的参数
+    console.log("verifying   contracts ......")
+    try {
+        await run("verify:verify", {
+            //第二个verify是相当于verify的一个功能，用来验证的
+            address: ContractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!") //如果异常消息中包含 "already verified"，则输出 "Already Verified!"，否则打印异常信息。
+        } else {
+            console.log(e)
+        }
+    }
 }
 mian()
     .then(() => process.exit(0))
